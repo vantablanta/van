279fad2022aacb61f20b54d1caab141b917e4bf9
vantablanta <njerimwanjiku@gmail.com>
2021-05-27 23:42:16 +0300

    String algos

--- file1.js
+++ file1.js
@@ -116,3 +116,33 @@ function truncateString(str, num) {
     return newStr
 }
 truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+/*
+Create a function that looks through an array arr and returns the
+ first element in it that passes a 'truth test'. This means that given an element x, 
+the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
+*/
+function findElement(arr, func) {
+    let num = 0;
+    for (let i = 0; i < arr.length; i++){
+        num = arr[i];
+        if (func(num)){
+        return num 
+        }
+    }
+return undefined 
+}
+findElement([1, 2, 3, 4], num => num % 2 === 0);
+
+/*
+Check if a value is classified as a boolean primitive. Return true or false.
+Boolean primitives are true and false.
+*/
+function booWho(bool) {
+    if (bool === true || bool === false){
+        return true
+    }
+return false;
+}
+booWho(null);
+    
